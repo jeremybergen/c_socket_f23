cfb7d7e2de17ef5e80b6adcf3ffd3cee63113097
Jeremy Bergen <jbergen@coloradomesa.edu>
2023-11-02 04:33:47 -0600

    friday 1103

--- multi-client.py
+++ multi-client.py
@@ -1,16 +1,44 @@
 import socket
-
+import errno
+import sys
 
 def main():
-    port = 1234
+    port = 1235
     host = "127.0.0.1"
+    headerlength = 10
+
+    username = input("What is your username? ")
+    usernamelength = len(username)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        
         s.connect((host, port))
+        s.setblocking(False)
+
+        # s.sendall(username.encode())
+        s.sendall(f"{usernamelength:<{headerlength}}{username}".encode())
+        # 7         jbergen
         while True:
-            message = "Hello"
-            s.send(message.encode())
-            break
+            message = input("Message to send: ")
+
+            if message:
+                print(f"Sending: {message}")
+                s.send(message.encode())
+            try:
+                while True:
+                    #receive messages
+                    recvMessage = s.recv(1024).decode()
+                    print(f"Received: {recvMessage}")
+                    continue
+            
+            except IOError as e:
+                if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+                    print(f"Reading error: {str(e)}")
+                    sys.exit()
+                continue
+            # message = "Hello"
+            # s.send(message.encode())
+            # break
 
 
 if __name__ == "__main__":
--- multi-server.py
+++ multi-server.py
@@ -1,42 +1,86 @@
 import socket
 import select
 
-
 def main():
-    port = 1234
-    host = ""
+    host = "127.0.0.1"
+    port = 1235
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((host, port))
-        s.listen(5)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serverSocket:
+        serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serverSocket.bind((host, port))
+        serverSocket.listen()
 
-        connectedSockets = [s]
-        clientList = {}
-
-        print(f"Listening for client connections")
+        socketList = [serverSocket]
+        clients = {}
 
         while True:
-            readSockets, _, exceptionSockets = select.select(connectedSockets, [], connectedSockets)
-
+            readSockets, writeSockets, errorSockets = select.select(socketList, [], socketList)
             for notifiedSocket in readSockets:
-                if notifiedSocket == s:
-                    clientSocket, clientAddress = s.accept()
-                    connectedSockets.append(clientSocket)
-                    clientList[clientSocket] = "jbergen"
-
-                    print(f"Accepted new connection from: {clientAddress}")
-                    # print(f"Current connections: {connectedSockets}")
+                print(f"notifiedSocket: {notifiedSocket}")
+                if notifiedSocket == serverSocket:
+                    clientSocket, clientAddress = notifiedSocket.accept()
+                    # userName = "jbergen" + str(len(socketList))
+                    header = clientSocket.recv(10).decode()
+                    headerlength = int(header)
+                    userName = clientSocket.recv(headerlength).decode()
+                    clients[clientSocket] = userName
+                    print(f"New client connected: {userName}:{clientAddress}")
+                    socketList.append(clientSocket)
+                    # new connection
                 else:
-                    try:
-                        message = notifiedSocket.recv(1024).decode()
-                        if len(message) == 0:
-                            continue
-                        print(f"Received from {notifiedSocket}: {message}")
-                    except:
-                        break
-            
+                    userName = clients[notifiedSocket]
+                    recvMessage = notifiedSocket.recv(1024)
+                    print(f"Received from {clientAddress}: {recvMessage.decode()}")
+                    for clientSocket in clients:
+                        if clientSocket != notifiedSocket:
+                            clientSocket.sendall(recvMessage)
+                    # existing connection
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+# import socket
+# import select
+
+
+# def main():
+#     port = 1234
+#     host = ""
+
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#         s.bind((host, port))
+#         s.listen(5)
+
+#         connectedSockets = [s]
+#         clientList = {}
+
+#         print(f"Listening for client connections")
+
+#         while True:
+#             readSockets, _, exceptionSockets = select.select(connectedSockets, [], connectedSockets)
+
+#             for notifiedSocket in readSockets:
+#                 if notifiedSocket == s:
+#                     clientSocket, clientAddress = s.accept()
+#                     connectedSockets.append(clientSocket)
+#                     clientList[clientSocket] = "jbergen"
+
+#                     print(f"Accepted new connection from: {clientAddress}")
+#                     # print(f"Current connections: {connectedSockets}")
+#                 else:
+#                     try:
+#                         message = notifiedSocket.recv(1024).decode()
+#                         if len(message) == 0:
+#                             continue
+#                         print(f"Received from {notifiedSocket}: {message}")
+#                     except:
+#                         break
